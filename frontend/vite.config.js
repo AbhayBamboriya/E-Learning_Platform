98528f84704951a16f5f32f2ddb318a742a78b27
AbhayBamboriya <bamboriya09@gmail.com>
2024-10-11 00:24:01 +0530

    base directory added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',  // Customize the directory for static assets like images
+    sourcemap: true,  // Generate source maps for easier debugging in production
+    minify: 'esbuild',  // Specify the minifier, options are 'esbuild' or 'terser'
+    chunkSizeWarningLimit: 500,  
+  }
 })
